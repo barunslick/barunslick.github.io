8a0aa13d5cf4d065af58170a63bbcbe08e4e6e94
barunslick <barunpradhan987@gmail.com>
2020-06-23 17:16:17 +0545

    Add exercise 4

---
+++ internship/assignments/Javascript/js_basics/Exercise:4/search.js
@@ -0,0 +1,20 @@
+var fruits = [
+    {id: 1, name: 'Banana', color: 'Yellow'},
+    {id: 2, name: 'Apple', color: 'Red'}
+]
+function searchByName(obj, req_name){
+    for (var each_fruit in obj){
+        if (obj[each_fruit].name === req_name){
+            return (obj[each_fruit]);
+        }
+    }
+}
+function searchByKey(obj, key_search,req_name){
+    for (var each_fruit in obj){
+        if (obj[each_fruit][key_search] === req_name){
+            return (obj[each_fruit]);
+        }
+    }
+}
+console.log(searchByName(fruits,'Apple'))
+console.log(searchByKey(fruits, 'color' ,'Yellow'))
