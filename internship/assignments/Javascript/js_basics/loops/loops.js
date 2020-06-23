098e4d7268e11ce6afa6ebeb844bc23981807c85
barunslick <barunpradhan987@gmail.com>
2020-06-23 17:18:36 +0545

    Rename files

--- internship/assignments/Javascript/js_basics/loops/loops.js
+++
@@ -1,7 +0,0 @@
-var num = window.prompt('Enter a number');
-my_div = document.getElementById('array_loop')
-for( var i = num; i> 0; i--){
-    p = document.createElement('p');
-    p.innerHTML = '*'.repeat(i);
-    my_div.appendChild(p);
-}
