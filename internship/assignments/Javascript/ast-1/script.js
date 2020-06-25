e2c1fee2c5f2b11474a719dab9a32e5f0b411af5
barunslick <barunpradhan987@gmail.com>
2020-06-25 14:32:51 +0545

    Add slide lef right functionality

--- internship/assignments/Javascript/ast-1/script.js
+++ internship/assignments/Javascript/ast-1/script.js
@@ -24,5 +24,44 @@ btnRight.style.right = '20px';
 carousalContainer.appendChild(btnLeft);
 carousalContainer.appendChild(btnRight);
 
-var image_width = images.clientWidth;
+var imageWidth = images.clientWidth;
 
+currentIndex = -1;
+btnRight.addEventListener('click',function(){   
+    var checkpoint_r =  (imageWidth * (currentIndex-1));
+    var currentPos_r = (imageWidth * currentIndex);
+    currentIndex--;
+    console.log(currentIndex)
+    function animate(){
+        var animation_id = window.requestAnimationFrame(animate);
+        carouselImageWrapper.style.left =  currentPos_r  + 'px';
+        currentPos_r -= 40;
+        if (currentPos_r < checkpoint_r){
+            cancelAnimationFrame(animation_id);
+            if(currentIndex == -6){
+                currentIndex = -1;
+                carouselImageWrapper.style.left = (imageWidth * currentIndex) + 'px';
+            }
+        }
+    }
+    animate();
+    
+})
+btnLeft.addEventListener('click',function(){   
+    var currentPos_l = imageWidth*currentIndex;
+    var checkpoint_l =  (currentPos_l + imageWidth);
+    currentIndex++;
+    function animateleft(){
+        var animation_id_l = window.requestAnimationFrame(animateleft);
+        carouselImageWrapper.style.left = currentPos_l + 'px';
+        currentPos_l += 40;
+        if (currentPos_l > checkpoint_l){
+            cancelAnimationFrame(animation_id_l);
+            if(currentIndex == 0){
+                currentIndex = -5;
+                carouselImageWrapper.style.left = (imageWidth * currentIndex) + 'px';
+            }
+        }
+    }
+    animateleft();
+})
