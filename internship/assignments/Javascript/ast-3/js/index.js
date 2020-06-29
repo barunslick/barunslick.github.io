e68c3e68d69e9b3dea15f15c418ecfc89919f2ec
barunslick <barunpradhan987@gmail.com>
2020-06-29 18:22:09 +0545

    Add checkcollision functions

--- internship/assignments/Javascript/ast-3/js/index.js
+++ internship/assignments/Javascript/ast-3/js/index.js
@@ -7,16 +7,57 @@ const pi = Math.PI;
 //get  CanvasRenderingContext2D object
 var ctx = canvas.getContext('2d');
 
-let noOfBalls = 100;
+let noOfBalls = 1000;
 let ballsArray = []
-for (let i = 0; i < noOfBalls; i++){
-  let radius = 20;
-  let x = getRandomIntRange(radius, window.innerWidth-radius);
-  let y = getRandomIntRange(radius, window.innerHeight-radius);
-  let newBall = new Ball(x, y, radius);
-  ballsArray.push(newBall);
-  newBall.draw();
+function init(){
+  for (let i = 0; i < noOfBalls; i++){
+    let radius = getRandomIntRange(2,5);
+    let x = getRandomIntRange(radius, window.innerWidth-radius); // this basically gives us an integer value from raidus to total width - raidus so that it doesnt pop up outside boundry
+    let y = getRandomIntRange(radius, window.innerHeight-radius);
+    let checker = false;
+    let velocity = {
+      x: getRandomIntRange(-5,5),
+      y: getRandomIntRange(-5,5)
+    };
+    if (ballsArray.length !==0){
+      checker = checkOverlap(x, y, radius, ballsArray)
+    }
+    if(checker){
+      i--;
+      continue;
+    }
+    ballsArray.push(new Ball(x, y, radius, velocity));
+    ballsArray[i].draw();
+  }
+  render();
 }
+
+init();
+
+function checkOverlap(x, y, radius,ballsArray){
+  for(let j = 0; j < ballsArray.length ; j++){
+    if ( getDistance(x, y , ballsArray[j].x, ballsArray[j].y ) < ( radius + ballsArray[j].radius )){
+    return true;
+    }
+  }
+  return false;
+}
+
+function render(){
+  window.requestAnimationFrame(render);
+  ctx.clearRect(0,0, window.innerWidth, window.innerHeight);
+  ballsArray.forEach( ball => {
+    
+    ball.updatePosition();
+    ball.checkWallCollision();
+    ball.checkBallCollision( ballsArray);
+    ball.draw();
+  });
+  
+}
+
+
+
 
 
 //gets a random value from given range
@@ -24,6 +65,13 @@ function getRandomIntRange(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//get distance between two balls
+function getDistance( x1, y1, x2, y2){
+  const xDistance = x2 - x1;
+  const yDistance = y2 - y1;
+  return Math.hypot(xDistance, yDistance)
 }
 
 
