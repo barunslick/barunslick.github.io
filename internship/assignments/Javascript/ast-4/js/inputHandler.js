a48abe37e3e58a915cc4e49d8a97214468e5586b
barunslick <barunpradhan987@gmail.com>
2020-07-01 01:20:09 +0545

    Add seperate class for eventHandling

---
+++ internship/assignments/Javascript/ast-4/js/inputHandler.js
@@ -0,0 +1,19 @@
+export default class InputHandler {
+
+	constructor (game){
+		this.game = game
+		this.createEventListener(this.game);
+	}
+
+	createEventListener(game) {
+		document.addEventListener( 'keydown', e => {
+			if (e.keyCode == 65){ //go left
+				game.userCar.goLeft();
+			}
+			else if(e.keyCode == 68){ //go right
+				game.userCar.goRight();
+			}
+		})
+	}
+
+}
