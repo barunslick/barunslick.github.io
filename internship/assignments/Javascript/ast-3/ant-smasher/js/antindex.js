acc7a9f03fcd6e04d506590c11d1fc50ebd65a5b
barunslick <barunpradhan987@gmail.com>
2020-06-30 08:18:35 +0545

    Add methods for creating and intitate collsion checking

--- internship/assignments/Javascript/ast-3/ant-smasher/js/antindex.js
+++ internship/assignments/Javascript/ast-3/ant-smasher/js/antindex.js
@@ -1,11 +1,58 @@
-my_ant = new Ant(600,20,3);
-my_ant.draw();
-
-function animate(){
-
-    my_ant.updatePosition();
-    my_ant.draw();
-    window.requestAnimationFrame(animate);
+let noOfAnts = 10;
+let antRadius = 30;
+let divHeight = document.querySelector('.ant-hive').clientHeight;
+let divWidth = document.querySelector('.ant-hive').clientWidth;
+var antArray = [];
+for (let i = 0; i < noOfAnts; i++) {
+	let checker = false;
+	let x = getRandomIntRange(antRadius, divWidth - antRadius);
+	let y = getRandomIntRange(antRadius, divHeight - antRadius);
+	console.log(x,y)
+	let velocity = {
+		x: getRandomIntRange(-1, 1),
+		y: getRandomIntRange(-1, 1)
+	}
+	if (antArray.length !== 0) {
+		checker = checkOverlap(x, y, antRadius, antArray);
+	}
+	if (checker) {
+		i--;
+		continue;
+	}
+	antArray.push(new Ant(x, y, velocity));
+	antArray[i].draw();
 }
 
+
+function animate() {
+	antArray.forEach(ant => {
+		ant.draw();
+		ant.updatePosition();
+		ant.checkWallCollision();
+		ant.checkAntCollision(antArray);
+	});
+	window.requestAnimationFrame(animate);
+}
 animate();
+
+function checkOverlap(x, y, radius, antArray) {
+	for (let j = 0; j < antArray.length; j++) {
+		if (getDistance(x, y, antArray[j].x, antArray[j].y) < (radius + antArray[j].radius)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function getRandomIntRange(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//get distance between two balls
+function getDistance(x1, y1, x2, y2) {
+	const xDistance = x2 - x1;
+	const yDistance = y2 - y1;
+	return Math.hypot(xDistance, yDistance)
+}
