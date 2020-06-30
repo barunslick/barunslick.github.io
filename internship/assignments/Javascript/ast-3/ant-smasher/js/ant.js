5aab92df063e335f239e73bd571a4bbb8b2bff6d
barunslick <barunpradhan987@gmail.com>
2020-06-30 08:17:01 +0545

    Add collision detection

--- internship/assignments/Javascript/ast-3/ant-smasher/js/ant.js
+++ internship/assignments/Javascript/ast-3/ant-smasher/js/ant.js
@@ -1,20 +1,76 @@
 var mainDiv = document.querySelector('.ant-hive');
 
-var Ant = function(x, y, velocity){
-    this.x = x;
-    this.y = y;
-    this.velocity = velocity;
-    this.element = document.createElement('div');
-    this.element.classList.add('ant');
-    mainDiv.appendChild(this.element)
+var Ant = function (x, y, velocity) {
+	this.x = x;
+	this.y = y;
+	this.radius = 30;
+	this.diameter = this.radius * 2;
+	this.velocity = velocity;
+	this.element = document.createElement('div');
+	this.element.classList.add('ant');
+	this.draw();
+	mainDiv.appendChild(this.element);
 }
 
-Ant.prototype.draw = function(){
-    this.element.style.bottom = this.x + 'px';
-    this.element.style.left = this.y + 'px';
+Ant.prototype.draw = function () {
+	this.element.style.top = this.y + 'px';
+	this.element.style.left = this.x + 'px';
 }
 
-Ant.prototype.updatePosition = function(){
-    this.x += 1;
-    this.y += 1;
+Ant.prototype.updatePosition = function () {
+	this.velocity.x = (this.velocity.x ==0) ? Math.random()*2: this.velocity.x;
+	this.velocity.y = (this.velocity.y ==0) ? Math.random()*2: this.velocity.y;
+	this.x += this.velocity.x;
+	this.y += this.velocity.y;
+}
+
+Ant.prototype.checkWallCollision = function () {
+	if (this.y + this.diameter >= divHeight) {
+		this.y = divHeight - this.diameter;
+		this.velocity.y = -this.velocity.y;
+	}
+	if (this.y <= 0) {
+		this.y = this.radius;
+		this.velocity.y = -this.velocity.y;
+	}
+	if (this.x + this.diameter >= divWidth) {
+		this.x = divWidth - this.diameter;
+		this.velocity.x = -this.velocity.x;
+	}
+	if (this.x <= 0) {
+		this.x = this.radius;
+		this.velocity.x = -this.velocity.x;
+	}
+}
+
+Ant.prototype.checkAntCollision = function(allAnts){
+	for (let i = 0; i < allAnts.length; i++) {
+		if (this == allAnts[i]) continue;
+		if (getDistance(this.x, this.y, allAnts[i].x, allAnts[i].y) <= ( 2* this.radius)) {
+			var tx = this.velocity.x;
+			var ty = this.velocity.y;
+			this.velocity.x = allAnts[i].velocity.x;
+			this.velocity.y = allAnts[i].velocity.y;
+			allAnts[i].velocity.x = tx;
+			allAnts[i].velocity.y = ty;
+			this.updatePosition();
+			allAnts[i].updatePosition();
+		}
+		if (getDistance(this.x, this.y, allAnts[i].x, allAnts[i].y) <= (this.radius + allAnts[i].radius)) {
+			this.hardSeperate(allAnts[i]);	
+		}
+
+	}
+}
+Ant.prototype.hardSeperate = function(stickyPartner){
+	var helpVelocity = -(this.velocity.x) * 2; //2 amplifies the splitting process
+	var count = 0;
+	while(getDistance(this.x, this.y, stickyPartner.x, stickyPartner.y) <= (this.radius + stickyPartner.radius)){
+		this.x += helpVelocity;
+		this.updatePosition();
+		count++;
+		if (count >100){ //prevent going infinite loop
+			break;
+		}
+	}
 }
