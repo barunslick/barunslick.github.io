b6a80d4c90f41fef7a12955f10a021ccf5c0957b
barunslick <barunpradhan987@gmail.com>
2020-06-30 16:43:20 +0545

    Refactor code

--- internship/assignments/Javascript/ast-4/js/index.js
+++ internship/assignments/Javascript/ast-4/js/index.js
@@ -1,5 +1,4 @@
 import Game from '/js/game.js';
-import Car from '/js/car.js';
 
 let canvas = document.getElementById('gameCanvas');
 let ctx = canvas.getContext('2d');
@@ -7,24 +6,14 @@ let ctx = canvas.getContext('2d');
 let gameHeight = canvas.clientHeight;
 let gameWidth = canvas.clientWidth;
 
-let roadImg = new Image;
-roadImg.src = "images/road.png";
 
-let yOffset = -gameHeight;
-roadImg.onload = () => {
-	requestAnimationFrame(startGame);
-}
+let game = new Game(ctx, gameWidth, gameHeight);
 
+requestAnimationFrame(startGame);
 function startGame(){
-	yOffset = (yOffset >= 0 )? -gameHeight : yOffset;
-	ctx.drawImage(roadImg, 0, yOffset, gameWidth , gameHeight);
-	yOffset += 15;
+	game.update();
 	requestAnimationFrame(startGame);
 }
-
-
-let game = new Game(gameWidth, gameHeight);
-game.start(ctx);
 
 
 
