c2c92c58d456c01303f500be92a9eb5bac722e58
barunslick <barunpradhan987@gmail.com>
2020-07-01 01:19:36 +0545

    Add seperate class for enemy car

---
+++ internship/assignments/Javascript/ast-4/js/enemyCar.js
@@ -0,0 +1,44 @@
+export default class EnemyCar {
+
+	constructor(game, lane) {
+		this.game = game;
+		this.ctx = this.game.ctx;
+		this.enemyCarImage = new Image;
+		this.enemyCarImage.src = '../images/' + getRandomIntRange(1,4) + '.png';
+		this.imageHeight = 100;
+		this.imageWidth = 60;
+		this.lane = lane;
+		this.x = (this.lane * this.game.gameWidth/3 + this.imageWidth/2) + 20; // 64 being image size;
+		this.y = -50;
+		this.speed = 2;
+		this.cross = false;
+	}
+
+	update(){
+		this.y += this.speed;
+		this.ctx.drawImage(this.enemyCarImage, this.x, this.y, this.imageWidth, this.imageHeight);
+		this.collisionDetection();
+		if (this.y > this.game.gameHeight){
+			this.cross = true;
+		}
+	}
+
+	increaseSpeed(){
+		this.speed += 10;
+	}
+
+	collisionDetection(){
+		this.game.enemyArray.forEach((enemy)=>{
+			if (enemy.y >= this.game.gameWidth/2 && enemy.y < (this.game.userCar.y + this.imageHeight)){
+				if (enemy.x == this.game.userCar.x && ((this.game.userCar.y - enemy.y ) < this.imageHeight)) this.game.pause = true;
+			}	
+		})
+	}
+
+}
+
+function getRandomIntRange(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
