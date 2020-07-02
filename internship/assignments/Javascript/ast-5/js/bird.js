d4aad07fe7bb669f0d75ec87ae91e95d71fa6787
barunslick <barunpradhan987@gmail.com>
2020-07-02 08:08:16 +0545

    Add gravity

--- internship/assignments/Javascript/ast-5/js/bird.js
+++ internship/assignments/Javascript/ast-5/js/bird.js
@@ -9,26 +9,47 @@ export default class Bird {
     this.y = 300;
     this.switch = false;
     this.frame = 0;
-    this.counter =10;
     this.totalFrames = 3;
+    this.gravity = 0.3;
+    this.speed = 0;
+    this.jump = 6;
     [this.sx, this.sy, this.width, this.height] = getBird();
     this.radius = this.width * 3 - 10;
+    this.toStopPosition = this.game.gameHeight - this.game.background.backgoundSpriteBottom[7];
   };
 
   update() {
-    let moveBy = (this.game.mainFrame % 10) == 0 ? 1 : 0;
-    this.frame += moveBy;
-    this.frame = this.frame % this.totalFrames;
-    if (this.y + this.height*3 <= this.game.gameHeight) {
-      if (this.game.gameStatus == 1) this.goDown();
-      this.ctx.drawImage(this.game.canvasSprite, this.sx + (this.frame * (11 + this.width)), this.sy , this.width, this.height, this.x, this.y, this.width * 3, this.height * 3);
-    }else {
-     this.ctx.drawImage(this.game.canvasSprite, this.sx , this.sy , this.width, this.height, this.x, this.y, this.width * 3, this.height * 3);
+    if (this.game.gameStatus == 0 || this.game.gameStatus == 1) {
+      let moveBy = (this.game.mainFrame % 10) == 0 ? 1 : 0;
+      this.frame += moveBy;
+      this.frame = this.frame % this.totalFrames;
+      
+    }
+
+    if (this.y + this.height * 3 <= this.toStopPosition) {
+      if (this.game.gameStatus == 1) {
+        this.goDown();
+      }
+      this.ctx.drawImage(this.game.canvasSprite, this.sx + (this.frame * (11 + this.width)), this.sy, this.width, this.height, this.x, this.y, this.width * 3, this.height * 3);
+    } else {
+      this.ctx.drawImage(this.game.canvasSprite, this.sx, this.sy, this.width, this.height, this.x, this.y, this.width * 3, this.height * 3);
+      this.game.gameStatus = 2;
     }
   };
 
-  goDown(){
-    this.y += 5;
+  goDown() {
+    this.speed += this.gravity;
+    this.y += this.speed;
+  }
+
+  moveUp() {
+    this.speed = -this.jump;
+  }
+
+  checkHitBotton() {
+    if (this.y + this.height >= this.game.gameHeight) {
+      this.game.gameStatus = 2;
+    }
   }
 
 };
