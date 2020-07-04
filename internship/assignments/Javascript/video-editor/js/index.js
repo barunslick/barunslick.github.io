caa32dfba67a74446e1dda2a3a5baff2ad8ea1a1
barunslick <barunpradhan987@gmail.com>
2020-07-04 23:27:23 +0545

    Preload videos

---
+++ internship/assignments/Javascript/video-editor/js/index.js
@@ -0,0 +1,44 @@
+let videoCurrent = document.getElementById('video-current');
+let videoCanvas = document.getElementById('all-video');
+let playButton = document.querySelector('.controls .playpause .play');
+
+let video_list = ['../assets/videos/Boat_13.mp4', '../assets/videos/2.mp4']
+let videoArray = [];
+
+function preloadVideos(srcs) {
+	function loadVideo(src) {
+		return new Promise(function (resolve, reject) {
+			var video = document.createElement('video');
+			video.onloadedmetadata = function () {
+				resolve(video);
+			};
+			video.onerror = video.onabort = function () {
+				reject(src);
+			};
+			video.src = src;
+		});
+	}
+	var promises = [];
+	for (var i = 0; i < srcs.length; i++) {
+		promises.push(loadVideo(srcs[i]));
+	}
+	return Promise.all(promises);
+}
+
+preloadVideos(video_list).then(function (videos) {
+	for(var i = 0; i < videos.length; i++){
+			let video = new Video(videos[i].src, videos[i].duration, videos[i]);
+			videoArray.push(video)
+	}
+	main();
+}, function (errImg) {
+	console.log('fail')
+});
+
+function main(){
+	
+console.log(videoArray)
+
+
+
+}
---
+++ internship/assignments/Javascript/video-editor/js/video.js
@@ -0,0 +1,7 @@
+class Video {
+	constructor(src, length ,ref) {
+		this.urlSource = src;
+		this.length = length;
+		this.ref = ref
+	}
+}
